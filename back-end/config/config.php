db85c848cccee62a2df52c774e32d79cbb97f910
emileCazalaa <emilecazalaa@gmail.com>
2025-03-19 16:28:48 +0100

    ajout fichier config pour connexion pdo et modif nom bd joueur et match

---
+++ back-end/config/config.php
@@ -0,0 +1,35 @@
+<?php
+
+function connectionBD(){
+
+    $host = "mysql-clubbasketbackend.alwaysdata.net";
+    $dbname = "clubbasketbasket_bd";
+    $username = "403724";
+    $password = "Agaboubou65$";
+
+    /*
+    $host = "localhost";
+    $dbname = "clubbasket_auth";
+    $username = "root";
+    $password = "root";
+*/
+
+
+///Connexion au serveur MySQL
+    try {
+        $linkpdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    } catch (Exception $e) {
+        die('Erreur  de connexion à la bd: ' . $e->getMessage());
+    }
+    return $linkpdo;
+
+}
+try {
+    $pdo = connectionBD();
+    echo "Connexion réussie à la base de données.";
+} catch (Exception $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
+}
+
+?>
+
