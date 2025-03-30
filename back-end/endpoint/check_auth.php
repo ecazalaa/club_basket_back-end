910d92dcdc9716fef90a6fd238ecf8b7a530e942
Romeo andriantsiferana <romeo.andriant@gmail.com>
2025-03-30 19:05:37 +0200

    maj du fichier sur jwt

--- back-end/endpoint/check_auth.php
+++ back-end/endpoint/check_auth.php
@@ -5,10 +5,7 @@ function check_auth() {
     // Récupération du token
     $token = get_bearer_token();
     
-<<<<<<< Updated upstream
-=======
-    
->>>>>>> Stashed changes
+
 
     if (!$token) {
         http_response_code(401);
